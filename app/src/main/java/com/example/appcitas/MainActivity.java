2fe7ab99761c012dfbc81ca8ae3fbba867ead86a
dnngmz <dannagomb@gmail.com>
2025-06-10 08:30:46 -0400

    hola 2

--- app/src/main/java/com/example/appcitas/MainActivity.java
+++ app/src/main/java/com/example/appcitas/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 //hola
+//hola2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
